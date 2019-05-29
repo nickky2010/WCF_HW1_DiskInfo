599983e634644a6dc337ee9e075e8782624a123e
Nikolay <nickky2010@mail.ru>
2019-05-30 01:50:15 +0300

    Add files via upload

---
+++ Service/DiskInfo/DiskInfo/DiskInfoService.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Security;
+using System;
+
+namespace DiskInfo
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class DiskInfoService : IDiskInfoContract
+    {
+        public string[] GetDiskInfo(string path)
+        {
+            string[] pathInfo = null;
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(path);
+                DirectoryInfo[] folders = dir.GetDirectories();
+                FileInfo[] files = dir.GetFiles();
+                int countName = folders.Length + files.Length;
+                if (countName > 0)
+                {
+                    pathInfo = new string[countName];
+                    for (int i = 0; i < folders.Length; i++)
+                    {
+                        pathInfo[i] = folders[i].Name;
+                    }
+                    for (int j = 0, i = folders.Length; i < countName; i++, j++)
+                    {
+                        pathInfo[i] = files[j].Name;
+                    }
+                }
+            }
+            catch(SecurityException)
+            {
+                pathInfo = null;
+            }
+            catch (ArgumentNullException)
+            {
+                pathInfo = null;
+            }
+            catch (ArgumentException)
+            {
+                pathInfo = null;
+            }
+            catch (PathTooLongException)
+            {
+                pathInfo = null;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                pathInfo = null;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                pathInfo = null;
+            }
+            return pathInfo;
+        }
+    }
+}
---
+++ Service/DiskInfo/Host/Program.cs
@@ -0,0 +1,26 @@
+﻿using DiskInfo;
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host = new ServiceHost(typeof(DiskInfoService), new Uri("http://localhost:8733/"));
+            host.AddServiceEndpoint(typeof(IDiskInfoContract), new WSHttpBinding(), "DiskInfoService/ep1");
+            //Добавляем поведение mex-точки ServiceMetadataBehavior в коллекцию поведений основного приложения службы
+            host.Description.Behaviors.Add(new ServiceMetadataBehavior());
+            //Добавляем конечную точку обмена метаданными в ведущее приложение службы   
+            host.AddServiceEndpoint(ServiceMetadataBehavior.MexContractName,
+                             MetadataExchangeBindings.CreateMexHttpBinding(),
+                             "mex");
+            host.Open();
+            Console.WriteLine("Service is waiting for customer requests...");
+            Console.ReadKey();
+            host.Close();
+        }
+    }
+}
